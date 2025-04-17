db86439fc80957883665d5e2e172d7dbfb54c18e
Soloiso <teddybadge@gmail.com>
2025-04-17 10:49:49 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -21,7 +21,7 @@ $result = mysqli_query($connection, $sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Dashboard</title>
-    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
     <script>
         function filterTable() {
             let input = document.getElementById("search").value.toLowerCase();
