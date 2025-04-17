ce51d4b07d2c91ede11cb1ec08c3d8ccf97cdc3e
Soloiso <teddybadge@gmail.com>
2025-04-17 10:50:09 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -40,7 +40,7 @@ $result = mysqli_query($conn, $sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Admin Panel</title>
-    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
     <script>
         function filterTable() {
             let input=document.getElementById("search").value.toLowerCase();
